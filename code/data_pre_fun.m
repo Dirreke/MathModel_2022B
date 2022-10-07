c3950550f4eed4e5c0968abbb0f69af4e3d38d91
葛明阳 <mingyang_ge@163.com>
2022-10-07 15:31:37 +0800

    fix bug

--- code/data_pre_fun.m
+++ code/data_pre_fun.m
@@ -2,8 +2,9 @@ function data = data_pre_fun(file)
 % file = "../data/dataA/dataA1.csv";
 f = fopen(file);
 fgetl(f);
-data_ori = textscan(f,'%s %s %d %d %d %s','delimiter',',');
-data(:,1) = str2num(char(replace(data_ori{6},'order',''))); % order
+data_ori = textscan(f,'%d %s %d %f %f %s','delimiter',',');
+% data(:,1) = str2num(char(replace(data_ori{6},'order',''))); % order
+data(:,1) = data_ori{1};    % id
 data(:,2) = data_ori{3};    % num
 data(:,3) = data_ori{4};    % length 
 data(:,4) = data_ori{5};    % width
@@ -13,8 +14,8 @@ data(:,5) = data(:,3) .* data(:,4); % area
 index = data(:,3) < data(:,4);
 tmp = data(index, 3);
 data(index, 3) = data(index, 4);
-data(index, 4) = data(index, 3);
-
+data(index, 4) = tmp;
+fclose(f);
 end
 
 
