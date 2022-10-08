43cc2992718a44777a6ceaea8ee7fe9b4391235a
NanaSwan <3180106166@zju.edu.cn>
2022-10-09 00:30:11 +0800

    fix

--- code/q2_tanlan.m
+++ code/q2_tanlan.m
@@ -8,10 +8,6 @@ max_item_number = 1000;
 max_item_area = 250e6;
 
 
-% tic
-% material_num;
-
-% toc
 %% 
 is_merge = true;
 iter_num = 0;
@@ -27,21 +23,26 @@ while is_merge
         for kk = 1:length(orders)
             % order_1 的材质k的排版数量
             order_1 = orders(kk);
-            index = (data_ori(:,9) == material_data(k,1)) & (data_ori(:,8) == order_1);
-            tmp_data_1 = data_ori(index,:);
-            tmp_num_1 = length(q1_FFF_fun(tmp_data_1,width,height));
+            index_1 = data_ori(:,8) == order_1;
+            tmp_data_1 = data_ori(index_1,:);
+            index_1_2 = tmp_data_1(:,9) == material_data(k,1);
+            tmp_data_1_2 = tmp_data_1(index_1_2,:);
+            
+            tmp_num_1 = length(q1_FFF_fun(tmp_data_1_2,width,height));
             for kkk = kk+1:length(orders)
                 % order_2 的材质k的排版数量
                 order_2 = orders(kkk);
-                index = (data_ori(:,9) == material_data(k,1)) & (data_ori(:,8) == order_2);
-                tmp_data_2 = data_ori(index,:);
+                index_2 = data_ori(:,8) == order_2;
+                tmp_data_2 = data_ori(index_2,:);
                 % 判断数量和面积是否满足要求
-                if size(tmp_data_1,1) + size(tmp_data_2,1) > max_item_number  || sum(tmp_data_1(:,3)) + sum(tmp_data_2(:,3)) > max_item_area
+                if size(tmp_data_1,1) + size(tmp_data_2,1) > max_item_number  || sum(tmp_data_1(:,5)) + sum(tmp_data_2(:,5)) > max_item_area
                     continue
                 end
-                tmp_num_2 = length(q1_FFF_fun(tmp_data_2,width,height));
+                index_2_2 = tmp_data_2(:,9) == material_data(k,1);
+                tmp_data_2_2 = tmp_data_2(index_2_2,:);
+                tmp_num_2 = length(q1_FFF_fun(tmp_data_2_2,width,height));
                 % order_1+2 的材质k的排版数量
-                tmp_data_12 = [tmp_data_1;tmp_data_2];
+                tmp_data_12 = [tmp_data_1_2;tmp_data_2_2];
                 tmp_num_12 = length(q1_FFF_fun(tmp_data_12,width,height));
                 if tmp_num_12 < tmp_num_1 + tmp_num_2
                     data_ori(data_ori(:,8) == order_2,8) = order_1;
@@ -58,7 +59,9 @@ while is_merge
         end
     end
     iter_num = iter_num + 1;
-    fprintf("No.%d iter merge: %d & %d. time passed %.2f seconds \n",iter_num, order_1,order_2,toc );
+    if is_merge
+        fprintf("No.%d iter merge: %d & %d. time passed %.2f seconds \n",iter_num, order_1,order_2,toc );
+    end
     
 end
     
