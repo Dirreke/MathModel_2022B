8997ca721762d0a7db579c6713199ed727cea116
葛明阳 <mingyang_ge@163.com>
2022-10-09 17:49:37 +0800

    fix

--- code/q2_main.m
+++ code/q2_main.m
@@ -7,6 +7,8 @@ file = [
     ];
 
 results = cell(length(file), 1);
+ratios = zeros(length(file), 1);
+num_platess = zeros(length(file), 1);
 parfor k = 1:length(file)
     [data_ori, material_index] = data_pre_fun(file(k));
     width = 1220;
@@ -15,7 +17,9 @@ parfor k = 1:length(file)
     max_item_area = 250e6;
     [batches,ratio,num_plates] = q2_tanlan(data_ori,width,height,max_item_number,max_item_area);
     results{k} = batches;
+    ratios(k) = ratio;
+    num_platess(k) = num_plates;
     save_to_file_fun(2, k,results{k}, material_index);
 %     draw_picture_fun(1, k);
-    fprintf("数据集dataB%d的最优组批排样策略的板材利用率为%.2f\n ",k,100*rates(k));
+    fprintf("数据集dataB%d的最优组批排样策略的板材利用率为%.2f\n ",k,100*ratios(k));
 end
