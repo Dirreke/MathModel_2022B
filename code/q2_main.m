49fa22919b2a4cb77a40a6d7b8e9ae93d710e87f
葛明阳 <mingyang_ge@163.com>
2022-10-09 16:58:13 +0800

    fix

--- code/q2_main.m
+++ code/q2_main.m
@@ -7,8 +7,8 @@ file = [
     ];
 
 results = cell(length(file), 1);
-for k = 1:length(file)
-    [data_ori, material_index] = data_pre_fun(file(3));
+parfor k = 1:length(file)
+    [data_ori, material_index] = data_pre_fun(file(k));
     width = 1220;
     height = 2440;
     max_item_number = 1000;
